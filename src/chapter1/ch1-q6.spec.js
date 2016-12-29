a10e2d1ce0fb11a586b8a72d0dcbb830dd1af7e2
nem035 <n.stojanovic035@gmail.com>
2016-12-29 02:58:31 -0500

    ch1-q6: Converted the code to be more readable and aligned to the book. Added second solution that only creates the extra string if necessary.

--- src/chapter1/ch1-q6.spec.js
+++ src/chapter1/ch1-q6.spec.js
@@ -1,17 +1,18 @@
-import { expect } from 'chai';
 import * as funcs from './ch1-q6';
+
+import { expect } from 'chai';
 
 for (let key in funcs) {
   let func = funcs[key];
 
-  describe('ch1-q6: ' + key, function() {
+  describe('ch1-q6: ' + key, function () {
 
-    it('returns input where null/undefined', function() {
+    it('returns input where null/undefined', function () {
       expect(func(null)).to.be.null;
       expect(func(undefined)).to.be.undefined;
     });
 
-    it('returns input where empty string', function() {
+    it('returns input where empty string', function () {
       expect(func('')).to.equal('');
     });
 
@@ -23,22 +24,23 @@ for (let key in funcs) {
       'ababababccab'
     ].forEach(arg => {
 
-      it(`returns input string where compression doesn't use less space: '${arg}'`, function() {
+      it(`returns input string where compression doesn't use less space: '${arg}'`, function () {
         expect(func(arg)).to.eql(arg);
       });
 
     });
 
     [
-      { arg: 'aaa', out: '3a' },
-      { arg: 'bbbbbb', out: '6b' },
-      { arg: 'abbbbbbc', out: '1a6b1c' },
-      { arg: 'aaabccc', out: '3a1b3c' },
-      { arg: 'hhellllllllooooo!', out: '2h1e8l5o1!' },
-      { arg: 'woorrrllllddddd', out: '1w2o3r4l5d' }
+      { arg: 'aaa', out: 'a3' },
+      { arg: 'bbbbbb', out: 'b6' },
+      { arg: 'abbbbbbc', out: 'a1b6c1' },
+      { arg: 'aaabccc', out: 'a3b1c3' },
+      { arg: 'hhellllllllooooo!', out: 'h2e1l8o5!1' },
+      { arg: 'woorrrllllddddd', out: 'w1o2r3l4d5' },
+      { arg: 'aaaaaaaaaaabbbbbbbbbbbb', out: 'a11b12' }
     ].forEach(context => {
 
-      it(`returns ${context.out} with string ${context.arg}`, function() {
+      it(`returns ${context.out} with string ${context.arg}`, function () {
         expect(func(context.arg)).to.eql(context.out);
       });
 
