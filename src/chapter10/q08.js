f7b00279333f57edcb97aad1bdc0b64cad496b3d
IAfanasov <iafanasov@hotmail.com>
2017-03-08 11:30:43 +0700

    ch10q08. Fixed question text

--- src/chapter10/q08.js
+++ src/chapter10/q08.js
@@ -1,5 +1,8 @@
-/*10.5 Sparse Search: Given a sorted array of strings that is interspersed 
-with empty strings, write a method to find the location of a given string.*/
+/*10.8 Find Duplicates: You have an array with all the numbers from 1to N, 
+where Nis at most 32,000.The array may have duplicate entries and you do not 
+know what N is. With only 4 kilobytes of memory available, how would you print 
+all duplicate elements in the array?
+*/
 
 export function getDuplicates(intArray) {
   const vector = {};
